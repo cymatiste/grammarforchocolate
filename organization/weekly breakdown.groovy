27f7bec2c1c6a3acfe1b99de4baaa5164a49867e
simrisek_sinkingship <sarah@sinkingship.ca>
2017-07-24 12:23:48 -0400

    ~ added a folder for organization / planning ~ started a weekly breakdown of goals

---
+++ organization/weekly breakdown.groovy
@@ -0,0 +1,46 @@
+PHASE 1 - EXPERIMENTS
+---------------------
+Overarching goal: try a bunch of available technologies to get an idea of the best overall tools to use.
+
+Week of JULY 23:	
+- 	'Adverbs, pure experimentation': 
+	downloaded a list of adverbs (to be inserted into the senteces where recipes are acted out), had loose plans of tagging them by emotional tone and by context relativity (e.g. at least, do they modify adjectives or verbs?). Got quickly overwhelmed by the possible different ways of organizing them, and decided that this is a goal better delayed until I have a more solid idea of higher level strategy, and available tools. 
+ 
+Week of JULY 30			
+- 	'TRACERY, LEVEL 1':
+	I think this might be very helpful for plotting out the overall structure of my recipe-based story snippets. Will Install it and test it with generating sentences from a minimal set of variables.
+
+Week of AUGUST 6		
+- 	'MYSTERY TOOLS':
+	Probably in previous week I will get some better notions of useful tools to help manage requirements for 	Compromise + TRACERY. This is where I will experiment with those.
+
+Week of AUGUST 13		
+-	' PHASE 2 PLANNING':
+	Assess work required to prepare corpus to be usable with the tools I have decided to work with. Fill in goals for PHASE 2.
+
+PHASE 2 - DATA PREPARATION
+---------------------------
+Overarching goal: prepare my corpus better to work well with the tech (Specific goals subject to change per results of PHASE 1)
+
+Week of AUGUST 20		
+-	Select recipes and clean sentences up to make them fit a set of workable templates
+
+Week of AUGUST 27		
+-	
+
+Week of SEPTEMBER 3		
+-	
+
+Week of SEPTEMBER 10	-		
+
+Week of SEPTEMBER 24	-		
+
+Week of OCTOBER 1		-	
+
+Week of OCTOBER 8		-	
+
+Week of OCTOBER 15		-	
+
+Week of OCTOBER 22		-	
+
+Week of OCTOBER 29		-
